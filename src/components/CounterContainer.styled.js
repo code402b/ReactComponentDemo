01c4fa69b9fc05872ecd8f7b15a59c9a935dc795
Anthony McGovern <mcgovern.anthony@gmail.com>
2022-08-04 22:17:07 -0400

    add styled-components

---
+++ src/components/CounterContainer.styled.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const CounterContainer = styled.div`
+
+  display: flex;
+`
+
+export default CounterContainer;
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,32 @@
 import { createRoot } from 'react-dom/client';
-import React from 'react'
+import React, { useState } from 'react'
+
+import CounterContainer from './components/CounterContainer.styled.js';
+
 
 const root = createRoot(document.getElementById('root'));
 
 function App() {
+
+  const [count, setCount] = useState(0);
+  const [inputNum, setInputNum] = useState(0);
+
   return (
-    <div>Hello Tragic World!</div>
+    <>
+      <h2>Counter</h2>
+      <CounterContainer>
+        <button onClick={() => setCount(count - 1)}>-</button>
+        <div>{count}</div>
+        <button onClick={() => setCount(count + 1)}>+</button>
+        <input
+          type="number"
+          value={inputNum}
+          onChange={(e) => setInputNum(parseInt(e.target.value))}
+          ></input>
+        <button onClick={() => setCount(inputNum)}>Set Count</button>
+      </CounterContainer>
+    </>
+
   )
 }
 
